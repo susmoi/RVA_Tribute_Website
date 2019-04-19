d77ff059f19e75391d94b39457d7517190002427
susmoi_homebase <whymeiee789@yahoo.com>
2019-04-19 10:58:19 -0400

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var num = 0;
 var txt = "The Top 7 Breweries in Richmond!";
 var speed = 100;
-
+  
 function typeWriter() {
   if (num < txt.length) {
     document.getElementById("type-text").innerHTML += txt.charAt(num);
